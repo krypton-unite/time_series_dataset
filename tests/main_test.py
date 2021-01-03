afdb4c8d754c75bc0033e5a18990aba1bda2c661
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-03 23:50:19 +0000

    :white_check_mark: Add tests.

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,4 +1,7 @@
 from tests.helpers import FlightsDataset
+import json
+from numpy import load
+import numpy as np
 
 def test_dataset_shape():
     flights_dataset = FlightsDataset()
@@ -6,3 +9,8 @@ def test_dataset_shape():
     assert flights_dataset.get_x_shape() == (1, 144, 2)
     fd = flights_dataset.make_future_dataframe(12)
     assert fd.shape == (1, 156, 2)
+    assert len(flights_dataset) == 1
+
+    dict_data = load('tests/helpers/data.npz')
+        
+    assert np.array_equal(flights_dataset[0][0],dict_data['arr_0']) and np.array_equal(flights_dataset[0][1],dict_data['arr_1'])
