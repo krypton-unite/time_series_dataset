b5b7938cbf06d771fccd73956299a9af32e64081
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-13 02:18:40 +0000

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup(
     name='time_series_dataset',
-    version='0.0.5',
+    version='0.0.6',
     description='Time series dataset for torch.',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
