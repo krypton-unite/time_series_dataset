74e97750bed280520ae1dd14f6b746a86cfc985b
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-14 01:40:51 +0000

    Bump version: 0.0.6 → 0.0.7

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,5 +1,5 @@
 python setup.py sdist bdist_wheel
-$version="0.0.6"
+$version="0.0.7"
 $files_to_handle_str="dist/time_series_dataset-$version*" 
 twine check $files_to_handle_str
 twine upload $files_to_handle_str
